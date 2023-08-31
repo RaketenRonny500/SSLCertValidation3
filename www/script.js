d8e1f1974c9d341a04552273226d3f4bc8b13cb3
RaketenRonny500 <c.bredau@protonmail.com>
2023-09-01 01:14:58 +0200

    Backend done

--- www/script.js
+++ www/script.js
@@ -1,91 +1,98 @@
 let token = '';
+const url = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`
 
 function setToken() {
-    token = document.getElementById('token').value;
-}
-
-async function apiCall(method, route, body = null) {
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-    };
-    const response = await fetch(`http://localhost:3000${route}`, {
-        method,
-        headers,
-        body: body ? JSON.stringify(body) : null
-    });
-
-    if (response.status === 204) return null;
-
-    if (response.ok) {
-        const text = await response.text();
-        return text ? JSON.parse(text) : null;
-    } else {
-        const text = await response.text();
-        throw new Error(`Failed to fetch: ${text}`);
-    }
-}
-
-
-async function getAllHosts() {
-    try {
-        const hosts = await apiCall('GET', '/host');
-        if (hosts) {
-            displayHosts(hosts);
-        }
-    } catch (error) {
-        console.error("Error fetching hosts:", error);
-    }
+    token = document.getElementById('token').value
 }
 
 async function createHost() {
-    const hostname = prompt('Enter new hostname:');
+    const hostname = prompt('Enter new hostname:')
     if (hostname) {
         try {
-            await apiCall('POST', '/host', { hostname });
-            await getAllHosts();
+            const response = await fetch(`${url}/host`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                body: JSON.stringify({ hostname })
+            })
+            if (!response.ok) {
+                throw new Error(`Failed to create host: ${await response.text()}`)
+            }
+            await fetchHosts();
         } catch (error) {
-            console.error("Error creating host:", error);
+            console.error("Error creating host:", error)
         }
     } else {
-        alert('Hostname is required to create a new host.');
+        alert('Hostname is required to create a new host.')
     }
 }
 
 async function deleteHost(id) {
-    const confirmDelete = confirm(`Are you sure you want to delete host with ID: ${id}?`);
-    if (confirmDelete) {
+    const deletePrompt = confirm(`Delete host with ID: ${id}?`);
+    if (deletePrompt) {
         try {
-            await apiCall('DELETE', `/host/${id}`);
-            await getAllHosts();
+            const response = await fetch(`${url}/host/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+            })
+            if (!response.ok) {
+                throw new Error(`Failed to delete host: ${await response.text()}`);
+            }
+            await fetchHosts();
         } catch (error) {
-            console.error("Error deleting host:", error);
+            alert(`Error deleting host: ${error}`);
         }
     }
 }
 
 async function updateHost(id, hostname) {
-    const newHostname = prompt(`Update hostname for ID ${id}. Current hostname is ${hostname}:`);
+    const newHostname = prompt(`Please enter a new hostname for ID ${id}:`)
     if (newHostname) {
         try {
-            await apiCall('POST', `/host/${id}`, { hostname: newHostname });
-            await getAllHosts();
+            const response = await fetch(`${url}/host`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                body: JSON.stringify({ hostname })
+            })
+            if (response.ok) {
+                await fetchHosts()
+            } else {
+                console.error(`Failed to update host: ${await response.text()}`);
+            }
+
         } catch (error) {
-            console.error("Error updating host:", error);
+            console.error("Error updating host:", error)
         }
     } else {
-        alert('New hostname is required for updating.');
+        alert('Please enter a hostname to update.')
     }
 }
 
-function displayHosts(hosts) {
-    const hostList = document.getElementById('hostList');
-    hostList.innerHTML = '';
-    hosts.forEach(host => {
-        const hostDiv = document.createElement('div');
-        hostDiv.innerHTML = `<span>ID: ${host.id}, Hostname: ${host.hostname}</span> 
-                             <button onclick="deleteHost(${host.id})">Delete</button>
-                             <button onclick="updateHost(${host.id}, '${host.hostname}')">Edit</button>`;
-        hostList.appendChild(hostDiv);
-    });
+async function fetchHosts() {
+    try {
+        const response = await fetch(`${url}/host`, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        const hosts = await response.json()
+        const hostList = document.getElementById('hostList')
+        hostList.innerHTML = ''
+        hosts.forEach(host => {
+            const hostDiv = document.createElement('div')
+            hostDiv.innerHTML = `<span>ID: ${host.id}, Hostname: ${host.hostname}</span> 
+                             <button onclick="deleteHost(${host.id})" class="btn btn-danger">Delete</button>
+                             <button onclick="updateHost(${host.id}, '${host.hostname}')" class="btn btn-warning">Edit</button>`
+            hostList.appendChild(hostDiv)
+        })
+    } catch (error) {
+        console.error("Error getting list of hosts.", error)
+    }
 }
